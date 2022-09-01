feac4409f9658010f9fd549b4f16435d983e3e7c
Vitaliya Tishchenko <vitaliya.tishchenko@mail.ru>
2022-09-01 12:17:35 +0300

    Initial Commit

---
+++ Employees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employees
+//
+//  Created by User on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
